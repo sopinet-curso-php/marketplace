3d1acaf963dc4ccf091a40098f5b113325aebe9e
Fernando <hidabe@gmail.com>
2016-07-10 11:41:06 +0000

    Add initial commit

---
+++ curso-assets/js/ciudades.js
@@ -0,0 +1,8 @@
+﻿var usastates = {
+    "malaga": "Málaga",
+    "cordoba": "Córdoba",
+    "huelva": "Huelva",
+    "santa-cruz": "Santa Cruz",
+    "sevilla": "Sevilla",
+    "mazagon": "Mazagón"
+}
