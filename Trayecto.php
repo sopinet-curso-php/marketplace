d83392ba98e545186953e6132f14f823bbcecf95
Fernando <hidabe@gmail.com>
2016-07-18 08:04:39 +0000

    Actualizado trayecto

--- Trayecto.php
+++ Trayecto.php
@@ -5,6 +5,7 @@ class Trayecto {
     var $origen;
     var $destino;
     var $calle;
+    var $fechaPublicacion;
     var $hora;
     var $precio;
     var $descripcion;
@@ -15,16 +16,22 @@ class Trayecto {
      * 
      * @return void
      **/ 
-    function llenarObjeto($p1, $p2, $p3, $p4, $p5, $p6, $p7, $p8, $p9) {
+    function llenarObjeto($p1, $p2, $p3, $p4, $p5, $p6, $p7, $p8, $p9, $p10) {
         $this->conductor = $p1;
         $this->avatar = $p2;
         $this->origen = $p3;
         $this->destino = $p4;
         $this->calle = $p5;
-        $this->hora = $p6;
-        $this->precio = $p7;
-        $this->descripcion = $p8;
-        $this->plazas = $p9;
+        $this->fechaPublicacion = new DateTime();
+        $this->fechaPublicacion->setTimestamp($p6);
+        $this->hora = $p7;
+        $this->precio = $p8;
+        $this->descripcion = $p9;
+        $this->plazas = $p10;
+    }
+    
+    function getFechaPublicacion() {
+        return $this->fechaPublicacion->format('d-m-Y');
     }
     
     /**
@@ -51,6 +58,48 @@ class Trayecto {
             return true;
         } else {
             return false;
+        }
+    }
+    
+    /**
+     * Copmrueba si el destino pasado por parámetro coincide con el de nuestro objeto ($this)
+     * 
+     * @return bool
+     **/
+    function tieneDestino($paramDestino) {
+        if ($paramDestino == $this->destino) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    /**
+     * Compara tanto si el origen coincide con el parámetro pasado o el destino
+     * 
+     * @return bool
+     **/
+    function buscar($param) {
+        if ($this->tieneDestino($param) || $this->tieneOrigen($param)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    /**
+     * Comprueba si la fecha actual está dentro del filtro de fecha seleccionado
+     * 
+     * @return bool
+     **/ 
+    function filtroFecha($param) {
+        if ($param == 0) {
+            return true;
+        } else {
+            $fechaHoy = new DateTime();
+            $diff = $fechaHoy->diff($this->fechaPublicacion);
+            $diffDias = $diff->format('%a');
+            return ($diffDias <= $param);
         }
     }
 }
