e7a6f29513cd50513c3dc9c83b437a4ff037168e
Fernando <hidabe@gmail.com>
2016-08-02 18:57:32 +0000

    Incorporado sistema de rutas, eliminados ficheros que no sirven

--- logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-require __DIR__ . "/configuracion.php";
-
-unset($_SESSION['facebook_access_token']);
-
-header("Location: main.php");
--- nuevoTrayecto.php
+++
@@ -1,6 +0,0 @@
-<?php
-require __DIR__ . "/configuracion.php";
-
-echo $twig->render('nuevoTrayecto.html.twig');
-
-?>
